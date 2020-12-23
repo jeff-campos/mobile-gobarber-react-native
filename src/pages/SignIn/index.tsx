d389b1a45c2f5060824d133508f07b2d288e1a3a
Admin <admin@jeff-campos.local>
2020-12-23 20:03:21 -0300

    Feat(Fonts): importing external sources

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 import Logo from '../../assets/logo.png';
 
@@ -9,6 +9,7 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={Logo} />
+      <Title>Faça seu Logon</Title>
     </Container>
   );
 };
